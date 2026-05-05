5c731f3fed5e5bab42b9275288d1057bd1686531
viviasantos <143008215+viviasantos@users.noreply.github.com>
2026-05-05 16:30:41 +0100

    .

--- sql_ultimate/init-data_aggregatefunction.sql
+++ sql_ultimate/init-data_aggregatefunction.sql
@@ -57,6 +57,92 @@ FROM sales.orders
 ORDER BY orderid;
 
 
+-- Exercise -- Find the total sales across all orders 
+-- addionally provide details such order id & order date
+SELECT 
+orderid,
+orderdate,
+SUM(sales) OVER() AS total_orders
+FROM sales.orders;
+
+
+-- Exercise -- Find the total sales for each product 
+-- addionally provide details such order id & order date
+SELECT 
+orderid,
+orderdate,
+productid,
+SUM(sales) OVER(PARTITION BY productid) AS total_sales_products
+FROM sales.orders;
+
+
+-- Exercise -- Find the total sales for each product and products
+-- addionally provide details such order id & order date
+SELECT 
+orderid,
+orderdate,
+productid,
+sales,
+SUM(sales) OVER() AS total_orders,
+SUM(sales) OVER(PARTITION BY productid) AS total_sales_products
+FROM sales.orders;
+
+-- Find the total sales for each combination of product and order status
+SELECT
+  sales,
+  orderstatus,
+  SUM(sales) OVER (PARTITION BY productid, orderstatus) AS total_sales
+FROM sales.orders;
+
+-- ORDER BY
+-- Rank each order based on their sales from highest to lowest,
+-- additionally provide details such orderid and orderdate
+
+SELECT
+orderid,
+orderdate,
+sales,
+RANK() OVER(ORDER BY sales DESC) AS ranksales
+FROM sales. orders;
+
+-- ROWS BETWEEN CURRENT ROW AND FOLLOWING - Defines a subset of rows within each window
+SELECT 
+orderid,
+orderdate,
+orderstatus,
+sales,
+SUM(sales) OVER(PARTITION BY orderstatus ORDER BY orderdate
+ROWS BETWEEN CURRENT ROW AND 2 FOLLOWING) AS total_sales
+FROM sales.orders;
+
+-- ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+SELECT 
+orderid,
+orderdate,
+orderstatus,
+sales,
+SUM(sales) OVER(PARTITION BY orderstatus ORDER BY orderdate
+ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS total_sales
+FROM sales.orders;
+
+-- ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+SELECT 
+orderid,
+orderdate,
+orderstatus,
+sales,
+SUM(sales) OVER(PARTITION BY orderstatus ORDER BY orderdate
+ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS total_sales
+FROM sales.orders;
+
+
+
+
+
+
+
+
+
 
 
 
